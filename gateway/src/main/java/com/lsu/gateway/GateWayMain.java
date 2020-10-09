ae7ce80e2af9f754741a0bfb30e8e9e8bd348a8a
wsuo <ws2821@yeah.net>
2020-10-09 19:44:19 +0800

    1.创建网关模块 2.在gateway增加system路由转发配置,并调试成功

---
+++ gateway/src/main/java/com/lsu/gateway/GateWayMain.java
@@ -0,0 +1,27 @@
+package com.lsu.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * 主启动类
+ *
+ * @Author wang suo
+ * @Date 2020/10/9 0009 18:35
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayMain {
+    private static final Logger LOG = LoggerFactory.getLogger(GateWayMain.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GateWayMain.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        LOG.info(env.getProperty("spring.application.name") + "\t启动成功!" + "\t地址:\thttp://localhost:" + env.getProperty("server.port"));
+    }
+}
