6d45f4481675c1fa79517d94b08d5c8bffb8f6ba
wsuo <ws2821@yeah.net>
2020-10-10 15:37:56 +0800

    登陆跳转

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login'
+import Admin from './views/admin'
 
 Vue.use(Router);
 
 export default new Router({
-   mode: 'history',
-   base: process.env.BASE_URL,
-   routes: [{
-       path: '*',
-       redirect: "/login",
-   }, {
-       path: '/login',
-       component: Login
-   }]
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '*',
+    redirect: "/login",
+  }, {
+    path: '/login',
+    component: Login
+  }, {
+    path: '/admin',
+    component: Admin
+  }]
 });
