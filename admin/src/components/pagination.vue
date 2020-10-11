a7254548ce3c601f3577689ad68c17d8aff14a95
wsuo <ws2821@yeah.net>
2020-10-11 14:17:43 +0800

    1.前端完成分页功能,包括样式和请求 2.可以自定义显示多少页

---
+++ admin/src/components/pagination.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="pagination" role="group" aria-label="分页">
+    <button type="button" class="btn btn-default btn-white btn-round"
+            v-bind:disabled="page === 1"
+            v-on:click="selectPage(1)">
+      1
+    </button>
+    <button type="button" class="btn btn-default btn-white btn-round"
+            v-bind:disabled="page === 1"
+            v-on:click="selectPage(page - 1)">
+      上一页
+    </button>
+    <button v-for="p in pages" v-bind:id="'page-' + p"
+            type="button" class="btn btn-default btn-white btn-round"
+            v-bind:class="{'btn-primary active':page === p}"
+            v-on:click="selectPage(p)">
+      {{p}}
+    </button>
+    <button type="button" class="btn btn-default btn-white btn-round"
+            v-bind:disabled="page === pageTotal"
+            v-on:click="selectPage(page + 1)">
+      下一页
+    </button>
+    <button type="button" class="btn btn-default btn-white btn-round"
+            v-bind:disabled="page === pageTotal"
+            v-on:click="selectPage(pageTotal)">
+      {{pageTotal||1}}
+    </button>
+    &nbsp;
+    <span class="m--padding-10">每页
+        <label>
+          <select v-model="size">
+              <option value="1">1</option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+          </select>
+        </label>
+      条，共【{{total}}】条
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "pagination",
+    // props 中的数据都是父组件给的
+    props: {
+      list: {
+        type: Function,
+        default: null
+      },
+      //显示的页码数
+      itemCount: Number
+    },
+    data() {
+      return {
+        // 总行数, 每页条数, 当前页码, 总页码, 显示的页码数组
+        total: 0,
+        size: 10,
+        page: 0,
+        pageTotal: 0,
+        pages: []
+      }
+    },
+    methods: {
+      render(page, total) {
+        let _this = this;
+        _this.page = page;
+        _this.total = total;
+        _this.pageTotal = Math.ceil(total / _this.size);
+        _this.pages = _this.getPageItems(_this.pageTotal, page, _this.itemCount || 5);
+      },
+
+      /**
+       * 查询某一页
+       * @param page 页码
+       */
+      selectPage(page) {
+        let _this = this;
+        if (page < 1) {
+          page = 1;
+        }
+        if (page > _this.pageTotal) {
+          page = _this.pageTotal;
+        }
+        if (this.page !== page) {
+          _this.page = page;
+          if (_this.page) {
+            _this.list(page)
+          }
+        }
+      },
+
+      /**
+       * 当前要显示在页面上的页码
+       *
+       * @param total 总页数
+       * @param current 当前页码
+       * @param itemCount 显示的页码数
+       * @returns {[]} 返回页码数组
+       */
+      getPageItems(total, current, itemCount) {
+        let items = [];
+        if (itemCount >= total) {
+          for (let i = 0; i < total; i++) {
+            items.push(i + 1);
+          }
+        } else {
+          let base = 0;
+          if (current - 0 > Math.floor((itemCount - 1) / 2)) {
+            base = Math.min(total, current - 0 + Math.ceil((itemCount - 1) / 2)) - itemCount;
+          }
+          for (let i = 0; i < itemCount; i++) {
+            items.push(base + i + 1);
+          }
+        }
+        return items;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .pagination {
+    vertical-align: middle !important;
+    font-size: 16px;
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+
+  .pagination button {
+    margin-right: 5px;
+  }
+
+  .btn-primary.active {
+    background-color: #2f7bba !important;
+    border-color: #27689d !important;
+    color: white !important;
+    font-weight: 600;
+  }
+</style>
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,100 +1,123 @@
 <template>
-  <table id="simple-table" class="table  table-bordered table-hover">
-    <thead>
-    <tr>
-      <th>ID</th>
-      <th>名称</th>
-      <th>课程ID</th>
-      <th>操作</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="chapter in chapters">
-      <td>{{chapter.id}}</td>
-      <td>{{chapter.name}}</td>
-      <td>{{chapter.courseId}}</td>
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button class="btn btn-xs btn-success">
-            <i class="ace-icon fa fa-check bigger-120"></i>
-          </button>
+  <div>
+    <p>
+      <button @click="getAll(1)" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh red2"></i>
+        刷新
+      </button>
+    </p>
+    <table id="simple-table" class="table  table-bordered table-hover">
 
-          <button class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-warning">
-            <i class="ace-icon fa fa-flag bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline pos-rel">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>课程ID</th>
+        <th>操作</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="chapter in chapters">
+        <td>{{chapter.id}}</td>
+        <td>{{chapter.name}}</td>
+        <td>{{chapter.courseId}}</td>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button class="btn btn-xs btn-success">
+              <i class="ace-icon fa fa-check bigger-120"></i>
             </button>
 
-            <ul
-              class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+            <button class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-warning">
+              <i class="ace-icon fa fa-flag bigger-120"></i>
+            </button>
+          </div>
+
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+              </button>
+
+              <ul
+                class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
 																			<span class="blue">
 																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
 																			</span>
-                </a>
-              </li>
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
 																			<span class="green">
 																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
 																			</span>
-                </a>
-              </li>
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
 																			<span class="red">
 																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
 																			</span>
-                </a>
-              </li>
-            </ul>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <!--
+        :list="getAll"
+
+        list: 是子组件暴露出来的一个回调方法;
+        getAll: 是父组件的 getAll 方法;
+      -->
+    <pagination ref="pagination" :list="getAll" :itemCount="8"/>
+  </div>
 </template>
 
 <script>
+  import Pagination from '../../components/pagination'
+
   export default {
     name: "chapter",
+    components: {
+      Pagination,
+    },
     data() {
       return {
         chapters: [],
       }
     },
     created() {
-      let _this = this;
-      _this.getAll();
     },
     mounted() {
-      // 操作 DOM 节点
+      let _this = this;
+      _this.$refs.pagination.size = 5;
+      _this.getAll(1);
     },
     methods: {
-      getAll() {
+      getAll(page) {
         let _this = this;
         _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-          page: 1,
-          size: 1
+          page: page,
+          size: _this.$refs.pagination.size,
         }).then(response => {
           _this.chapters = response.data.list;
+          // 渲染子组件
+          _this.$refs.pagination.render(page, response.data.total);
         })
       }
     }
