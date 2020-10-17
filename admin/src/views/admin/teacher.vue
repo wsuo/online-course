775d8bd13cafd993d057af23fd258666a194130b
wsuo <ws2821@yeah.net>
2020-10-17 14:23:30 +0800

    文件上传功能开发: springboot+vue 基本的文件上传功能,上传讲师头像

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -71,9 +71,9 @@
                 </div>
               </div>
               <div class="form-group">
-                <label for="image" class="col-sm-2 control-label">头像</label>
+                <label for="file-upload-input" class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <input v-model="teacher.image" id="image" class="form-control">
+                  <input type="file" id="file-upload-input" v-on:change="uploadImage">
                 </div>
               </div>
               <div class="form-group">
@@ -209,6 +209,17 @@
             }
           });
         });
+      },
+      uploadImage() {
+        let _this = this;
+        let formData = new window.FormData();
+        // key: "file" 必须和后端 controller 参数名一致
+        formData.append('file', document.querySelector('#file-upload-input').files[0]);
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then(response => {
+          Loading.hide();
+          let resp = response.data;
+        })
       }
     }
   }
---
+++ file/src/main/java/com/lsu/file/controller/admin/UploadController.java
@@ -0,0 +1,49 @@
+package com.lsu.file.controller.admin;
+
+import com.lsu.server.dto.ResponseDto;
+import com.lsu.server.util.UuidUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 上传文件控制器
+ *
+ * @Author wang suo
+ * @Date 2020/10/17 0017 13:52
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/admin")
+public class UploadController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UploadController.class);
+    public static final String BUSINESS_NAME = "文件上传";
+
+    @RequestMapping("/upload")
+    public ResponseDto upload(@RequestParam MultipartFile file) {
+        LOG.info("文件上传开始: {}", file);
+        LOG.info("文件名称: {}", file.getOriginalFilename());
+        LOG.info("文件大小: {}", file.getSize());
+
+        // 保存文件到本地
+        String filename = file.getOriginalFilename();
+        String key = UuidUtil.getShortUuid();
+        String fullPath = "D:/fileUpload/imooc/teacher/" + key + "-" + filename;
+        File dest = new File(fullPath);
+        try {
+            file.transferTo(dest);
+            LOG.info("文件保存路径: {}", dest.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ResponseDto responseDto = new ResponseDto();
+        return responseDto;
+    }
+}
