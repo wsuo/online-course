eee5aabb23a69da34cae1883b8f74f8347ebb20f
wsuo <ws2821@yeah.net>
2020-10-23 17:58:26 +0800

    用户管理与登录: 基于 vue 路由的登录拦截

--- admin/src/main.js
+++ admin/src/main.js
@@ -29,6 +29,22 @@ Object.keys(filter).forEach(key => {
   Vue.filter(key, filter[key]);
 });
 
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(item => {
+    return item.meta.loginRequire;
+  })) {
+    let loginUser = Tool.getLoginUser();
+    if (Tool.isEmpty(loginUser)) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
 new Vue({
   router,
   render: h => h(App),
