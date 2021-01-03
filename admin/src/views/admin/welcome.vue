0f53237c4f9dcd3b2f04ebb1f1bb58274ef9d703
wsuo <ws2821@yeah.net>
2021-01-03 10:57:53 +0800

    控制台欢迎页面开发:增加顶部统计

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -1,5 +1,44 @@
 <template>
-  <div><h1>你好,欢迎进入控制台</h1></div>
+  <div>
+    <div class="space-12"></div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="center">
+          <span class="btn btn-app btn-sm btn-light no-hover">
+            <span class="line-height-1 bigger-170 blue"> 1,411 </span>
+					  <br/>
+					  <span class="line-height-1 smaller-90"> 课程 </span>
+					</span>
+          <span class="btn btn-app btn-sm btn-yellow no-hover">
+            <span class="line-height-1 bigger-170"> 32 </span>
+						<br/>
+						<span class="line-height-1 smaller-90"> 大章 </span>
+					</span>
+          <span class="btn btn-app btn-sm btn-pink no-hover">
+            <span class="line-height-1 bigger-170"> 4 </span>
+            <br/>
+            <span class="line-height-1 smaller-90"> 小节 </span>
+          </span>
+          <span class="btn btn-app btn-sm btn-grey no-hover">
+            <span class="line-height-1 bigger-170"> 23 </span>
+            <br/>
+            <span class="line-height-1 smaller-90"> 讲师 </span>
+          </span>
+          <span class="btn btn-app btn-sm btn-success no-hover">
+            <span class="line-height-1 bigger-170"> 7 </span>
+            <br/>
+            <span class="line-height-1 smaller-90"> 报名 </span>
+          </span>
+          <span class="btn btn-app btn-sm btn-primary no-hover">
+            <span class="line-height-1 bigger-170"> 55 </span>
+            <br/>
+            <span class="line-height-1 smaller-90"> 评论 </span>
+          </span>
+        </div>
+        <div class="space-12"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
