083df81df5a40c55e378a67ebd9864a0602c3458
wsuo <ws2821@yeah.net>
2021-01-03 13:10:37 +0800

    控制台欢迎页面开发:增加销售量统计报表显示

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -84,14 +84,81 @@
         <div class="hr hr2 hr-double"></div>
         <div class="space-12"></div>
       </div>
-    </div>
+      <div class="col-sm-12">
+        <div class="widget-box transparent">
+          <div class="widget-header widget-header-flat">
+            <h4 class="widget-title lighter">
+              <i class="ace-icon fa fa-signal"></i>
+              销售量
+            </h4>
+          </div>
+          <div class="widget-body">
+            <div class="widget-main padding-4">
+              <div id="sales-charts"></div>
+            </div><!-- /.widget-main -->
+          </div><!-- /.widget-body -->
+        </div><!-- /.widget-box -->
+        <div class="space-12"></div>
+        <div class="hr hr2 hr-double"></div>
+        <div class="space-12"></div>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
   </div>
 </template>
 
 <script>
   export default {
     name: "welcome",
-    mounted() {
+    mounted: function () {
+      let _this = this;
+      _this.drawSalesCharts();
+    },
+    methods: {
+      drawSalesCharts() {
+        // 假数据: 随机生成数组
+        let d1 = [];
+        for (let i = 0; i < 30; i++) {
+          d1.push([i + 1, 2000 + Math.floor((Math.random() * 100) + 1)]);
+        }
+        let d2 = [];
+        for (let i = 0; i < 30; i++) {
+          d2.push([i + 1, 1900 + Math.floor((Math.random() * 100) + 1)]);
+        }
+        let sales_charts = $('#sales-charts').css({'width': '100%', 'height': '220px'});
+        $.plot("#sales-charts", [{
+          label: "最近30天",
+          data: d1
+        }, {
+          label: "上一周期",
+          data: d2
+        }], {
+          hoverable: true,
+          shadowSize: 0,
+          series: {
+            lines: {
+              show: true
+            },
+            points: {
+              show: true
+            }
+          },
+          xaxis: {
+            tickLength: 0
+          },
+          /* 分成多少个点: 最小值: 最大值: 小数点个数*/
+          yaxis: {
+            ticks: 10,
+            min: 1850,
+            max: 2150,
+            tickDecimals: 0
+          },
+          grid: {
+            backgroundColor: {colors: ["#fff", "#fff"]},
+            borderWidth: 1,
+            borderColor: '#555'
+          }
+        });
+      }
     }
   }
 </script>
