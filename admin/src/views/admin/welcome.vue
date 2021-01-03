fc8c5958af34cb122e16b100ddc5298afba52585
wsuo <ws2821@yeah.net>
2021-01-03 12:36:56 +0800

    控制台欢迎页面开发:增加评论/新增注册会员/课程报名统计显示

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -3,7 +3,7 @@
     <div class="space-12"></div>
     <div class="row">
       <div class="col-xs-12">
-        <div class="center">
+        <div class="text-center">
           <span class="btn btn-app btn-sm btn-light no-hover">
             <span class="line-height-1 bigger-170 blue"> 1,411 </span>
 					  <br/>
@@ -35,6 +35,53 @@
             <span class="line-height-1 smaller-90"> 评论 </span>
           </span>
         </div>
+        <div class="space-12"></div>
+        <div class="hr hr2 hr-double"></div>
+        <div class="space-12"></div>
+      </div>
+      <div class="col-xs-12 infobox-container">
+        <div class="infobox infobox-green">
+          <div class="infobox-icon">
+            <i class="ace-icon fa fa-comments"></i>
+          </div>
+
+          <div class="infobox-data">
+            <span class="infobox-data-number">32</span>
+            <div class="infobox-content">新增评论</div>
+          </div>
+
+          <div class="stat stat-success">8%</div>
+        </div>
+
+        <div class="infobox infobox-blue">
+          <div class="infobox-icon">
+            <i class="ace-icon fa fa-twitter"></i>
+          </div>
+
+          <div class="infobox-data">
+            <span class="infobox-data-number">11</span>
+            <div class="infobox-content">新注册会员</div>
+          </div>
+
+          <div class="badge badge-success">
+            +32%
+            <i class="ace-icon fa fa-arrow-up"></i>
+          </div>
+        </div>
+
+        <div class="infobox infobox-pink">
+          <div class="infobox-icon">
+            <i class="ace-icon fa fa-shopping-cart"></i>
+          </div>
+
+          <div class="infobox-data">
+            <span class="infobox-data-number">8</span>
+            <div class="infobox-content">课程报名</div>
+          </div>
+          <div class="stat stat-important">4%</div>
+        </div>
+        <div class="space-12"></div>
+        <div class="hr hr2 hr-double"></div>
         <div class="space-12"></div>
       </div>
     </div>
